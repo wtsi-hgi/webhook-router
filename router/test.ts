8b37bb5bc8e31f1022dbe8f02cecc499e6a1d45d
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-10-13 15:27:41 +0100

    Finished writing router tests

--- router/test.ts
+++ router/test.ts
@@ -1,5 +1,6 @@
 import cp = require("child_process");
 import {routeRequest} from "./router";
+import http = require("http")
 
 function logOutput(error: Error, stdout: string, stderr: string){
     if(error !== null){
@@ -16,30 +17,29 @@ function logOutput(error: Error, stdout: string, stderr: string){
 }
 
 it("routes correctly", (finish) => {
-    var processes = <cp.ChildProcess[]>[];
-    require("./router.js");
-    processes.push(cp.exec("python ../config/server.py --debug --port 8001 --host 127.0.0.1", logOutput))
+    var got_route = false;
+    http.createServer((req, res) => {
+        res.end("response");
+        got_route = true;
+    }).listen(3001);
 
-    setTimeout(() => {
-        var token = cp.execSync(`curl 127.0.0.1:8001/add-route -H "Content-Type: application/json" -X POST -d '{"name":"route","destination": "127.0.0.1:8002"}' -s | jq -r ".token"`)
-            .toString().replace("\n", "");
-        
-        var stdout = "";
-        processes.push(cp.exec("nc -l 8002", (error, _stdout, stderr) => {/*
-            if(error !== null){
-                throw error;
+    routeRequest(<any>{
+        rawHeaders: {
+            x: "y"
+        },
+        addListener: (type: string, func: () => void) => {
+            if(type == "end"){
+                func()
             }
-
-            console.error(stderr);
-            console.log(_stdout);*/
-            stdout += _stdout;
-            if(stdout.search("header: value") != -1){
+        }
+    }, <any>{
+        write: () => {
+        },
+        writeHead: () => {
+        },
+        end: () => {
+            if(got_route)
                 finish()
-            }
-        }))
-        
-        processes.push(cp.exec(`curl 127.0.0.1:8080/${token} -X POST -H "header: value"`, logOutput))
-    }, 3000)
-
-    setTimeout(() => processes.forEach(process => process.kill()), 5000)
+        }
+    }, "http://127.0.0.1:3001")
 });
