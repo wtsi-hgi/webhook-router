f843769fd43f61462de236c6e1e58f305ab5baa1
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-10-17 10:26:57 +0100

    Use http-proxy

--- router/router.ts
+++ router/router.ts
@@ -2,6 +2,7 @@ import http = require('http');
 import https = require('https');
 import url = require("url");
 import fetch from "node-fetch";
+import httpProxy = require("http-proxy")
 
 const configServer = "http://127.0.0.1:8081"
 
@@ -23,68 +24,6 @@ class RoutingException extends Error{
     }
 }
 
-export function routeRequest(request: http.IncomingMessage, response: http.ServerResponse, destination: string){
-    return new Promise((resolve, reject) => {
-        let parsedUrl = url.parse(destination);
-        
-        var httpProtocol = http; // for the type checker
-        if (parsedUrl.protocol == "http:"){
-            httpProtocol = http;
-        }
-        else if (parsedUrl.protocol == "https:"){
-            httpProtocol = <any>https;
-        }
-        else{
-            throw new RoutingException("Invalid client protocol", 400);
-        }
-
-        // have to use `.rawHeaders` as `.headers` makes all properties lower case 
-        var headers = <any>{};
-        for(var i = 0;i < request.rawHeaders.length;i += 2){
-            let headerKey = request.rawHeaders[i];
-            if(headerKey.toLowerCase() != "host"){ // let node.js set this
-                // hope there are no duplicate headers, otherwise they'll be overwritten
-                headers[headerKey] = request.rawHeaders[i + 1];
-            }
-        }
-
-        var forwardedReq = httpProtocol.request(<any>{
-            ...parsedUrl,
-            method: "POST",
-            headers: {
-                ...headers
-            }
-        }, (forwardedResp) => {
-            forwardedResp.addListener("data", (chunk) => {
-                response.write(chunk, 'binary');
-            })
-
-            forwardedResp.addListener("end", () => {
-                response.end();
-                resolve();
-            })
-
-            response.writeHead(<number>forwardedResp.statusCode, forwardedResp.headers);
-        });
-
-        forwardedReq.addListener("error", (e) => {
-            reject(new RoutingException("Cannot connect to server", 500, e.toString()))
-        })
-
-        request.addListener('data', (chunk) => {
-            forwardedReq.write(chunk, 'binary');
-        });
-
-        request.addListener('end', () => {
-            forwardedReq.end();
-        });
-
-        request.addListener('error', (e) => {
-            reject(e);
-        })
-    })
-}
-
 async function getDestinationFromToken(token: string){
     var configServerResp = await fetch(`${configServer}/routes/${token}`)
     try{
@@ -98,10 +37,14 @@ async function getDestinationFromToken(token: string){
         throw new RoutingException("Config server error: " + configServerJSON.error, configServerResp.status);
     }
 
-    return configServerJSON.destination;
+    return <string>configServerJSON.destination;
 }
 
-async function handleRequest(request: http.IncomingMessage, response: http.ServerResponse){
+let proxy = httpProxy.createProxyServer({
+    changeOrigin: true
+})
+
+function handleRequest(request: http.IncomingMessage, response: http.ServerResponse, onError: (error: any) => any){
     // see if the request is correct and extract the token
     // should be POST request of the form /{token}
     let requestUrl = url.parse(<string>request.url);
@@ -115,25 +58,34 @@ async function handleRequest(request: http.IncomingMessage, response: http.Serve
         return;
     }
 
-    await routeRequest(request, response, await getDestinationFromToken(path[0]));
+    getDestinationFromToken(path[0]).then((destination) => {
+        proxy.web(request, response, {
+            target: destination
+        }, (error) => {
+            onError(error)
+        })
+    }).catch((error) => {
+        onError(error)
+    })
 }
 
+proxy.on("end", (req, res, proxyRes) => {
+    // TODO: log successful route
+})
+
 http.createServer(async (request, response) => {
-    try{
-        await handleRequest(request, response);
-    }
-    catch(e){
-        if (e instanceof RoutingException){
-            if (e.logMessage){
-                console.log(e.logMessage);
+    handleRequest(request, response, (error) => {
+        if (error instanceof RoutingException){
+            if (error.logMessage){
+                console.log(error.logMessage);
             }
-            writeError(e.message, e.httpErrorCode, response);
+            writeError(error.message, error.httpErrorCode, response);
         }
         else{
             writeError("Internal Server Error", 500, response);
-            console.log(e);
+            console.log(error);
         }
-    }
+    });
 }).listen(8080);
 
 process.on('uncaughtException', function (err) {
