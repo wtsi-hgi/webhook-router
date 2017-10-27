c180f1db7255cfb2790ab0c642a1c31dcda89316
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-10-27 15:45:10 +0100

    Cleanup and make build script

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,3 +1,30 @@
+/* Originally copied from https://github.com/Microsoft/TypeScript-Vue-Starter/blob/a587fed60ff0d3d26c7ce1466b28c5b40953b036/webpack.config.js
+MIT License
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+*/
+
+var webpack = require('webpack')
+
+
 module.exports = {
     entry: './index.ts',
     output: {
@@ -17,11 +44,11 @@ module.exports = {
                 loader: 'vue-loader',
                 options: {
                     loaders: {
-                    // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-                    // the "scss" and "sass" values for the lang attribute to the right configs here.
-                    // other preprocessors should work out of the box, no loader config like this necessary.
-                    'scss': 'vue-style-loader!css-loader!sass-loader',
-                    'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this necessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
                     }
                     // other vue-loader options go here
                 }
@@ -31,13 +58,34 @@ module.exports = {
                 loader: 'ts-loader',
                 exclude: /node_modules/,
                 options: {
-                appendTsSuffixTo: [/\.vue$/],
+                    appendTsSuffixTo: [/\.vue$/],
                 }
             },
             {
                 test: /\.css$/,
-                use: [ 'css-loader' ]
+                use: ['css-loader']
             }
         ]
     }
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
 }
