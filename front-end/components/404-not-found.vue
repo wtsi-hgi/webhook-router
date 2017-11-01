cdb417115fc36ec33551130d0805d9ac3678780a
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-11-01 15:04:21 +0000

    Added front-end 404 not found page

---
+++ front-end/components/404-not-found.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+<whr-navbar>
+    <span class="divider">&nbsp; </span>
+    <router-link to="/">
+        <button type="button" class="btn btn-outline-secondary">
+            Home
+        </button>
+    </router-link>
+    <span class="mr-auto"></span>
+    <slot name="logoutButton"></slot>
+</whr-navbar>
+    <br />
+    <div class="container-fluid">
+        <p>Error: Invalid URL</p>
+    </div>
+</div>
+</template>
+<script>
+import NavBarComponent from "./whr-navbar.vue";
+
+export default {
+    components: {
+        "whr-navbar": NavBarComponent
+    },
+}
+</script>
+
