c2f00dfec704e50905a97ed08b4aa13b96ebe251
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-12-04 15:39:03 +0000

    Make copying functions more obvious

--- front-end/src/components/modify-route.vue
+++ front-end/src/components/modify-route.vue
@@ -40,6 +40,7 @@
             <div class="form-section">
                 <code ref="routeLocation" @click="selectRouteLocation">{{routingServerLocation}}/{{token}}</code>
                 <div style="display: inline-block" data-trigger="manual" ref="copyButton" title="Copied!" @click="copyRouteLocation">
+                <button type="button" class="btn btn-outline-secondary btn-sm">
                     <!--
                         From https://octicons.github.com/icon/clippy/
                         MIT License
@@ -65,7 +66,9 @@
                         SOFTWARE.
 
                     -->
-                <svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg>
+                <svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path class="clipboard-icon" fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg>
+                <span style="display:inline-block;vertical-align: text-bottom;padding-left: 1px;">Copy Route</span>
+                </button>
                 </div>
             </div>
             <br />
@@ -165,9 +168,15 @@
 </div>
 </div>
 </template>
-<style scoped>
+<style>
 .form-section{
     padding-left: 10px
+}
+.clipboard-icon{
+    fill: rgb(134, 142, 150);
+}
+button:hover .clipboard-icon{
+    fill: #fff;
 }
 </style>
 
