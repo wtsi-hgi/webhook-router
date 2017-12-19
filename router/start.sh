8446fc586bad63a36d844067cd6096bddb64ce74
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-12-19 11:28:39 +0000

    Correct start.sh

--- router/start.sh
+++ router/start.sh
@@ -2,4 +2,4 @@
 (sleep 45 && /fluent-bit/bin/fluent-bit -c conf/fluent-bit.conf) &
 
 # Run the routing server
-node router.js --port 80 --host 0.0.0.0 --configServer $(CONFIGSERVER_LOCATION)
+node router.js --port 80 --host 0.0.0.0 --configServer $CONFIGSERVER_LOCATION
