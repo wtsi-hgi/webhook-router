75e39ad5af5c564f14c74debc83ff2f66b6db957
ThomasHickman <Thomas.Hickman42@gmail.com>
2018-07-06 16:04:03 +0100

    Add transpile only to webpack config

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -50,6 +50,7 @@ module.exports = {
                 exclude: /node_modules/,
                 options: {
                     appendTsSuffixTo: [/\.vue$/],
+                    transpileOnly: true
                 }
             },
             {
